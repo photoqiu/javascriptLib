934e83bf6e0ab712b1348bb61e7eb5e35526bef8
photoqiu <photoqiu@163.com>
2013-12-08 01:39:34 +0800

    dot.js is js

---
+++ dot.js
@@ -0,0 +1,20 @@
+(function() {
+    var doc = document;
+    var win = window;
+    var config = {};
+    function dot () {
+        
+    }
+    dot.prototype = {
+        init = function() {
+        
+        },
+        getSystem = function() {
+        
+        },
+        setSystem = function() {
+        
+        }
+    };
+
+})();
